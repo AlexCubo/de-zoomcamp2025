adac188070c7909ac66ead09c96e38ab48a8bed7
Alessandro Cubeddu <alessandro.cubeddu@yahoo.co.uk>
2025-01-14 20:07:54 +0100

    edited ingest_data.py

--- 01_Docker-Terraform/docker/ingest_data.py
+++ 01_Docker-Terraform/docker/ingest_data.py
@@ -1,27 +1,47 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import os
+import argparse
 import pandas as pd
 from sqlalchemy import create_engine
 from time import time
 
-# Connection to postgres
-engine = create_engine('postgresql://root:root@localhost:5432/ny_taxi')
 
-engine.connect()
+def main(params):
+    user = params.user
+    password = params.password
+    host = params.host
+    port = params.port
+    db = params.db
+    table_name = params.table_name
 
-df_iter = pd.read_csv('./data/green_tripdata_2019-10.csv', iterator=True, chunksize=100000)
+    green_data = './data/green_tripdata_2019-10.csv'
+    zone_lookup = './data/taxi_zone_lookup.csv'
 
-df = next(df_iter)
+    # Connection to postgres
+    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
 
-df.lpep_pickup_datetime = pd.to_datetime(df.lpep_pickup_datetime)
-df.lpep_dropoff_datetime = pd.to_datetime(df.lpep_dropoff_datetime)
+    engine.connect()
 
-df.head(n=0).to_sql(name='green_taxi_data', con=engine, if_exists='replace')
+    #load taxi lookup data
+    zone_lookup_df = pd.read_csv(zone_lookup)
+    df.head(n=0).to_sql(name='taxi_zone_lookup', con=engine, if_exists='replace')
+    zone_lookup_df.to_sql(name='taxi_zone_lookup', con=engine, if_exists='replace')
 
-df.to_sql(name='green_taxi_data', con=engine, if_exists='append')
+    # load green data
+    df_iter = pd.read_csv(green_data, iterator=True, chunksize=100000)
 
-try:
+    df = next(df_iter)
+
+    df.lpep_pickup_datetime = pd.to_datetime(df.lpep_pickup_datetime)
+    df.lpep_dropoff_datetime = pd.to_datetime(df.lpep_dropoff_datetime)
+
+    df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
+
+    df.to_sql(name=table_name, con=engine, if_exists='append')
+
+    try:
     i=0
     while True:
 
@@ -32,19 +52,29 @@ try:
         df.lpep_pickup_datetime = pd.to_datetime(df.lpep_pickup_datetime)
         df.lpep_dropoff_datetime = pd.to_datetime(df.lpep_dropoff_datetime)
 
-        df.to_sql(name='green_taxi_data', con=engine, if_exists='append')
+        df.to_sql(name=table_name, con=engine, if_exists='append')
         t_end = time()
 
         print(f'Inserted Chunk number {i}, took {t_end - t_start}')
 
-except StopIteration:
+    except StopIteration:
     print(f'All Chunks of data ingested into postgres database.')
+    
 
+if __name__ == '__main__':
 
-#Load zone_lookup table
+    parser = argparse.ArgumentParser(
+        description='Ingest CSV ny taxi data')
 
-zone_lookup_df = pd.read_csv('./data/taxi_zone_lookup.csv')
+    # user, password, host,database, port, table
+    # url of csv files
+    parser.add_argument('user', help='username for postgres')
+    parser.add_argument('password', help='password for postgres')
+    parser.add_argument('host', help='host for postgres')
+    parser.add_argument('db', help='database in postgres')
+    parser.add_argument('port', help='postgres port')
+    parser.add_argument('table_name', help='name of the table')
 
-df.head(n=0).to_sql(name='taxi_zone_lookup', con=engine, if_exists='replace')
-
-zone_lookup_df.to_sql(name='taxi_zone_lookup', con=engine, if_exists='replace')
+    args = parser.parse_args()
+    
+    main(args)
